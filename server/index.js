5c950beee2415eb84ccdc46a1f2f834fbc5acc3e
Boraco <melgonza88@gmail.com>
2026-03-10 00:06:20 -0400

    fix: configurar trust proxy para express-rate-limit

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
+app.set('trust proxy', 1) // Confía en el primer proxy (Nginx)
 app.use(express.json())
 
 const limiter = rateLimit({
